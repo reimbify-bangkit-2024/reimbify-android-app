03f9a7a36f980791a7211c29d020387b94bb48f1
Shelma Salsabila <13521115@std.stei.itb.ac.id>
2024-11-20 19:31:53 +0700

    feature:notification

--- ReimbifyApp/app/src/main/java/com/example/reimbifyapp/MainActivityUser.kt
+++ ReimbifyApp/app/src/main/java/com/example/reimbifyapp/MainActivityUser.kt
@@ -1,35 +1,65 @@
 package com.example.reimbifyapp
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.reimbifyapp.databinding.ActivityMainUserBinding
 
 class MainActivityUser : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainUserBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainUserBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
+        // Set up toolbar
+        setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+        // Set up Navigation
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // Configure AppBar
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_dashboard, R.id.navigation_history, R.id.navigation_add_request, R.id.navigation_profile, R.id.navigation_setting
+                R.id.navigation_dashboard,
+                R.id.navigation_history,
+                R.id.navigation_add_request,
+                R.id.navigation_profile,
+                R.id.navigation_setting
             )
         )
+
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        binding.navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_notifications -> {
+                Toast.makeText(this, "Notifications clicked", Toast.LENGTH_SHORT).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
